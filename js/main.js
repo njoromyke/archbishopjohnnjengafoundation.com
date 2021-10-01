ad9d93d8b16e7e1da9b7584800defa6563091a43
unknown <mykesoftware@gmail.com>
2021-10-01 15:17:21 +0300

    Minor changes on fonts and images

--- js/main.js
+++ js/main.js
@@ -63,6 +63,23 @@ $(document).ready(function(){
         easing: easeInOutQuad,
         a11y: false
       });
+      $(function () {
+
+        $('#myBtn').goTop({
+      
+          // top offset
+          scrollTop: 100,
+      
+          // scroll speed in milliseconds
+          scrollSpeed: 1000,
+      
+          // fade in/out speed in milliseconds
+          fadeInSpeed: 1000,
+          fadeOutSpeed: 500
+          
+        })
+      
+      })
 });
 
 var myVar;
@@ -76,22 +93,3 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
