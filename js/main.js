8d1c100f9c58718b656f218fd04af943fd4b3030
Michael Njoroge <56773318+ngahika01@users.noreply.github.com>
2021-10-11 12:25:10 +0300

    Updated foundation page

--- js/main.js
+++ js/main.js
@@ -1,62 +1,22 @@
-$(document).ready(function(){
-  
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var top = $(window).scrollTop();
+    if (top >= 60) {
+      $("nav").addClass("bg-color");
+    } else {
+      if ($("nav").hasClass("bg-color")) {
+        $("nav").removeClass("bg-color");
+      }
+    }
+  });
+  $(".gallery-item").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:0,
-                nav:false,
-                loop:false
-            },
-            600:{
-                items:1,
-                nav:false,
-                loop:true
-            },
-            1000:{
-                items:1,
-                nav:false,
-                loop:true,
-                autoHeight:false,
-            }
-        },
-        autoplay:true,
-        autoplayHoverPause:true,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        autoHeight:true,
-        
-
-
-
-    });
-
- 
-        $(window).scroll(function(){
-            var top = $(window).scrollTop();
-            if(top>=60){
-                $('nav').addClass('bg-color');
-            }else{
-                if($('nav').hasClass('bg-color')){
-                    $('nav').removeClass('bg-color');
-    
-                }
-    
-            }
-          });
-    $('.gallery-item').magnificPopup({
-        type: 'image',
-        gallery:{
-          enabled:true
-        },
-        
-      });
-
-      new WOW().init();
-      
+  new WOW().init();
 });
 
 var myVar;
@@ -69,4 +29,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
-
